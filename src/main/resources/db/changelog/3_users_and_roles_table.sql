ce9d93c12b8c23110d77c0b52e374a6733981b83
nkempegowda <nkempegowda@gmail.com>
2025-04-10 08:34:18 +0530

    EXPENSE-1: user expense issue fix

--- src/main/resources/db/changelog/3_users_and_roles_table.sql
+++ src/main/resources/db/changelog/3_users_and_roles_table.sql
@@ -1,20 +1,15 @@
 --liquibase formatted sql
 
 --changeset kempegowdafs:5
-CREATE TABLE users (
+CREATE TABLE expense_users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     username VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created_by BIGINT DEFAULT -1,
-    updated_by BIGINT DEFAULT -1,
-    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+    password VARCHAR(255) NOT NULL);
 
 --changeset kempegowdafs:6
-CREATE TABLE roles (
+CREATE TABLE expense_roles (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
@@ -24,6 +19,6 @@ CREATE TABLE user_roles (
     user_id BIGINT,
     role_id BIGINT,
     PRIMARY KEY (user_id, role_id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (role_id) REFERENCES roles(id)
+    FOREIGN KEY (user_id) REFERENCES expense_users(id),
+    FOREIGN KEY (role_id) REFERENCES expense_roles(id)
 );
